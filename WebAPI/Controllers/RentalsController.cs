42e78be230ff262aa7440820c3d60b10e8ace19a
mustafa barlas <mustafa.barlas1146@gmail.com>
2023-06-06 00:27:04 +0300

    WebAPI added

---
+++ WebAPI/Controllers/RentalsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // ATRIBUTE : 
+
+    //IOC   Inversion Of Control
+    public class RentalsController : ControllerBase
+    {
+        private IRentalService _rentalService;
+
+
+        public RentalsController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        [HttpGet]
+        public List<Rental> GetAll()
+        {
+            
+           
+            var result = _rentalService.GetAll();
+            return result.Data;
+        }
+    }
+}
